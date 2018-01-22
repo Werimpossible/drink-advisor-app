64ca90e95bfa5b4c1fa09391ad0a541dcc247ecb
Andrea Gutierrez <andrea.gurod@gmail.com>
2018-01-22 13:04:57 -0600

    #3 comenzando a nombrar funciones base

--- js/app.js
+++ js/app.js
@@ -5,64 +5,35 @@ $(document).ready(loadPage);
 //Mandar elementos HTML al Js
 var $inputSearcher = $("#bar-seacher");
 
-
-//Aqui meteras modales y searcher del filtro
-function loadPage() {
-    $nameInput.keyup(validateContact);
-    $phoneInput.keyup(validateContact);
-    $("#searcher").keyup(filterContacts);
+// Función base de las funciónes
+function loadPage(){
+    loadSplashView(); //Activa la función del splash
+    loadMainView(); //Activa la función del main
+    $('#bars-container').append(datamap.map(getBarsInHtml));
+    $filterInput.keyup(filterBars)//Se activa al presionar una vez una tecla
 }
 
-//Este nos funciona para cuando el usuario agregue el valor
-var name = $nameInput.val();
-var phone = $phoneInput.val();
-$("#modal1").modal("close");
+//función para el splash
+function loadSplashView() {
+    //Definición del tiempo de activación del splash
+    setTimeout(function() {
+        $("main").fadeIn(1500);
+    }, 3000);
+}
 
-//Este bloque debes de personalizarlo dependiendo de como hagas tu container de bares
+//función para mostrar contenedor de opciones de bares, acabando el splash
+function loadMainView(){
+    setTimeout(function() {
+        $("main").fadeIn(1500);
+    }, 3000);
+}
 
-function paintContactsInHtml(contact) {
-    //crear elementos con DOM
-    var $newContact = $("<article />", {
-        "class": "card-panel hoverable"
-    });
-    var $containerContactName = $("<h4 />");
-    var $deleteContactButton = $("<button type='button' />");
-    var $removeIcono = $("<i />", {
-        "class": "material-icons"
-    });
-    var $containerContactPhone = $("<p />");
+//función de mostrar "las opciones filtradas"
+function getBarsInHtml(bar){
 
-    //Agregamos atributos y eventos a los elementos creados en el dom
-    $deleteContactButton.addClass("btn right");
-    $removeIcono.text("delete");
-    $deleteContactButton.click(removeContact);
+//nombramiento de variables
+var nameBar = bar.nombre;
+var imgBar = bar.img;
+var $nearbyBar = $('section />');
 
-    //Asignando valores
-    $deleteContactButton.append($removeIcono);
-    $containerContactName.text(contact.name);
-    $containerContactPhone.text(contact.phone);
-
-    $newContact.append($containerContactName);
-    $newContact.append($deleteContactButton);
-    $newContact.append($containerContactPhone);
-
-
-
-    //agregamos lo que creamos con el Dom a un elemento existente del html
-
-    function filterContacts() {
-        var searchContact = $("#searcher").val().toLowerCase();
-        if ($("#searcher").val().trim().length > 0) {
-            var filteredContacts = contacts.filter(function (contact) {
-                return contact.name.toLowerCase().indexOf(searchContact) >= 0;
-            });
-            $("#publish-contacts").empty();
-            filteredContacts.forEach(function (contact) {
-                paintContactsInHtml(contact);
-            });
-        } else {
-            $("#publish-contacts").empty();
-            contacts.forEach(function (contact) {
-                paintContactsInHtml(contact);
-            });
-            }
+}
